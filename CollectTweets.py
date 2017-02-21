2f263651a553500dc9c252e83c35663ba8489da8
cglynn <cglynn@albany.edu>
2017-02-21 12:45:17 -0500

    Add files via upload

--- CollectTweets.py
+++ CollectTweets.py
@@ -3,116 +3,166 @@
 Created on Wed Feb 15 08:52:26 2017
 
 @author: cglynn
-Collect Tweets including current Video names store to file
-Collect Random Sample of Tweets
+Collect Tweets including current Video names store to file retrievedTweets.data
+Collect Random Sample of Tweets and store to file randomSampleTweets.data
+Query terms saved to query.data
 """
 
 # -*- coding: utf-8 -*-
 
-import tweepy, sys, json, hashlib, time, urllib
+import tweepy, sys, json, time, urllib2, sets, string, stop_words
 
 #==============================================================================
-# Setup tweepy API
+# Setup tweepy API and Global variables
 #==============================================================================
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-consumer_key='PeH7lROp4ihy4QyK87FZg' 
-consumer_secret='1BdUkBd9cQK6JcJPll7CkDPbfWEiOyBqqL2KKwT3Og' 
-access_token_key='1683902912-j3558MXwXJ3uHIuZw8eRfolbEGrzN1zQO6UThc7'
-access_token_secret='e286LQQTtkPhzmsEMnq679m7seqH4ofTDqeArDEgtXw'
+consumer_key='Okf8rnmarIctPnfjwfqMj9Fpf' 
+consumer_secret='HakszG0KyyjYJxA0TraalLXY6bxBTEQBst5ZgHBw5IyoZg4WAM' 
+access_token_key='832742214849531905-MwZ6NMLvO0zNf0XYewBS1VwAgBxj8aC'
+access_token_secret='omcKXM1kOJOIMDclejLqdY8xwCwdmwgGPjnTpCAc90OW6'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token_key, access_token_secret)
 myApi = tweepy.API(auth)
+movieList = []
+numberOfResults = 1000
+tweetList = []
 ###############################################################################
 
 #==============================================================================
-# Setup Fandango API
-# API Code from: https://developer.fandango.com/page/API__Sample_Code_Python
+# Setup Gracenot API for current movie titles
+#Allows up to 50 calls per day; 2 calls per second.
 #==============================================================================
-class FandangoApiManager(object):
-
-    def __init__(self):
-        
-        self.FandangoApiManager = [ ]
-
-    def Sha256Encode(self, stringToEncode):
-
-        s = hashlib.sha256();
-        s.update(stringToEncode)
-        result = s.hexdigest()
-
-        return result
-
-    def BuildAuthorizationParameters(self, apiKey, sharedSecret):
-
-        paramsToEncode = "{0}{1}{2}".format(apiKey, sharedSecret, int(time.time()))
-        encodedParams = self.Sha256Encode(paramsToEncode)
-        result = "apikey={0}&sig={1}".format(apiKey, encodedParams)
-        
-        return result
-
-    def GetResponse(self, parameters):
-
-        baseUri = "http://api.fandango.com"
-        apiVersion = "1"
-
-        apiKey = "sg2jbzbj2sb6ftyyk3pzrn76"
-        sharedSecret = "cfTBKsZB7n"
-
-        authorizationParameters = self.BuildAuthorizationParameters(apiKey, sharedSecret)
-        requestUri = "{0}/v{1}/?{2}&{3}".format(baseUri, apiVersion, parameters, authorizationParameters)
-
-        response = urllib.urlopen(requestUri)
-        
-        result = response.read()
-        
-        return result
-
+#consumer_key = 'cuscgszmmfg3cwenss7bj273'
+consumer_key = 'wxsswsn7m4zhy823g957jbp4'
+baseUrl = "http://data.tmsapi.com/v1.1"
+showtimesUrl = baseUrl + '/movies/showings?'
+zipCode = "12222"
+date = time.strftime("%Y-%m-%d")
+parameters = 'startDate=' + date + '&zip=' + zipCode + '&api_key=' + consumer_key
+movieUrl = showtimesUrl + parameters
 ###############################################################################
 
 #Retrieve Tweets based on current movie titles         
 def rest_query_movieTitlesTweets():
-    query = "FIFTY SHADES DARKER "
+    global movieList
+    numMovies = 1
+    if(len(movieList) > 1):
+        numMovies = len(movieList)
+    resultCount = numberOfResults / numMovies
+    numRepititions = resultCount / 100    
+    if(numRepititions < 1):
+        numRepititions = 1
+    for movieTitle in movieList:
+        addTweets(movieTitle, numRepititions)
+    with open("retrievedTweets.data", 'w') as file:
+        file.write(json.dumps(tweetList))
+            
+#Add tweets to list
+def addTweets(queryString, numRepititions):
     geo = "42.6525,-73.7572,9mi" # City of Albany
     MAX_ID = None
-    tweets = myApi.search(q=query, geocode=geo, count=100, max_id = MAX_ID)
-    for it in range(2): # Retrieve up to 200 tweets
-        tweets = myApi.search(q=query, geocode=geo, count=100, max_id = MAX_ID)
+    global tweetList
+    tweets = myApi.search(q=queryString, geocode=geo, count=100, max_id = MAX_ID)
+    for it in range(numRepititions):
+        tweets = myApi.search(q=queryString, geocode=geo, count=100, max_id = MAX_ID)
         if tweets:
             MAX_ID = tweets[-1].id
-            print MAX_ID, len(tweets)
-            print tweets[-1].text
+            for tweet in tweets:
+                tweetList.append(convertTweepyObjToDict(tweet))
+    
 
 #Retrieve movie Titles
 def rest_query_movieTitle():
+    request = urllib2.Request(movieUrl)
+    movieSet = sets.Set()
+    global movieList
+    movieDictionary = dict()
+    global queryList
+    try:
+        response = urllib2.urlopen(request)
+        movies = response.read()
+        movieDictionary = json.loads(movies)
+    except urllib2.URLError, e:
+        print 'Error connecting to Movie API: ' , e
+    for movie in movieDictionary:
+        title = removeStopWords(removePunctuation(movie['title']))
+        wordsInTitle = title.split()
+        for words in wordsInTitle:
+            movieSet.add(words) 
+    movieList = list(movieSet)
+    with open("query.data", 'w') as file:
+        file.write(json.dumps(movieList))  
     
-    api = FandangoApiManager()
+
+#Remove punctuation
+def removePunctuation(text):
+    for p in string.punctuation:
+        text = text.replace(p,'')
+    return text
+
+#Remove Stopwords
+def removeStopWords(textInput):
+    import datetime
+    now = datetime.datetime.now()
+    stopWords = stop_words.get_stop_words('english')
+    stopWords.append(now.year)
+    stopWords.append('2')
+    stopWords.append('3d')
+    stopWords.append('la')
+    wordsList = textInput.split()
+    for word in wordsList:
+        try:
+            stopWords.index(word.lower())
+            wordsList.remove(word)
+        except Exception:
+            pass
+    return ' '.join(wordsList)
     
-    zipCode = "12222";
-    parameters = "op=moviesbypostalcodesearch&postalcode={0}".format(zipCode)
-
-    responseFromServer = api.GetResponse(parameters)
-
-    print responseFromServer
-
-    # process responseFromServer...
 
 #Retrieve Random Sample
 def rest_query_randomSample():
     numResults = 100
     geo = "42.6525,-73.7572,9mi" # City of Albany
     MAX_ID = None
-    tweets = myApi.search(geocode=geo, count=numResults, max_id = MAX_ID)
-    for it in range(2): # Retrieve up to 200 tweets
-        tweets = myApi.search(geocode=geo, count=numResults, max_id = MAX_ID)
+    tweetList = []
+    tweets = myApi.search(geocode=geo,count=numResults, max_id = MAX_ID)
+    for it in range(20): # Retrieve up to 2000 tweets
+        tweets = myApi.search(geocode=geo,count=numResults, max_id = MAX_ID)
         if tweets:
             MAX_ID = tweets[-1].id
-            print MAX_ID, len(tweets)
-            print tweets[-1].text
+            for tweet in tweets:
+                tweetList.append(convertTweepyObjToDict(tweet))
+    with open("randomSampleTweets.data", 'w') as file:
+        file.write(json.dumps(tweetList))       
+
+def convertTweepyObjToDict(tweepyObject):
+    return {
+         'contributors': tweepyObject.contributors, 
+         'truncated': tweepyObject.truncated, 
+         'text': tweepyObject.text,
+#         'in_reply_to_status_id': tweepyObject.in_reply_to_status_id,
+         'id': tweepyObject.id,
+         'retweeted': tweepyObject.retweeted,
+         'coordinates': tweepyObject.coordinates,
+         'source': tweepyObject.source,
+#         'in_reply_to_screen_name': tweepyObject.in_reply_to_screen_name,
+         'id_str': tweepyObject.id_str,
+         'retweet_count': tweepyObject.retweet_count,
+#         'in_reply_to_user_id': tweepyObject.in_reply_to_user_id,
+         'favorited': tweepyObject.favorited,
+         'source_url': tweepyObject.source_url, 
+         'geo': tweepyObject.geo, 
+#         'in_reply_to_user_id_str': tweepyObject.in_reply_to_user_id_str, 
+         'created_at': str(tweepyObject.created_at), 
+#         'in_reply_to_status_id_str': tweepyObject.in_reply_to_status_id_str
+#         ,'place': tweepyObject.place
+    }
+    
 
 if __name__ == '__main__':
-#    rest_query_randomSample()
+    rest_query_randomSample()
     rest_query_movieTitle()
     rest_query_movieTitlesTweets()
